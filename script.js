9e7c83d23d11bd14c646c5abf7f44ab8a8a41762
Gurveer <81466930+killswitch01@users.noreply.github.com>
2025-02-14 09:00:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-document.getElementById("noBtn").addEventListener("mouseover", function() {
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", function() {
     const x = Math.random() * (window.innerWidth - 100);
     const y = Math.random() * (window.innerHeight - 50);
 
-    this.style.position = "absolute";
     this.style.left = `${x}px`;
     this.style.top = `${y}px`;
 });
